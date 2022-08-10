db483ec34cd11eb7d3aabd853aed958f1e57d1c8
nhtrung1980 <nhtrung1980@gmail.com>
2022-08-10 21:57:08 +0700

    update

---
+++ src/api/term/content-types/term/lifecycles.js
@@ -0,0 +1,30 @@
+const slugify = require("slugify");
+module.exports = {
+    beforeCreate(event) {
+        const { data } = event.params;
+        if (data.Name) {
+            data.Slug = slugify(data.Name, {
+                replacement: '-',  // replace spaces with replacement character, defaults to `-`
+                remove: undefined, // remove characters that match regex, defaults to `undefined`
+                lower: true,      // convert to lower case, defaults to `false`
+                strict: false,     // strip special characters except replacement, defaults to `false`
+                locale: 'vi',       // language code of the locale to use
+                trim: true         // trim leading and trailing replacement chars, defaults to `true`
+            });
+        }
+    },
+    beforeUpdate(event) {
+        const { data } = event.params;
+        
+        if (data.Name) {
+            data.Slug = slugify(data.Name, { 
+                replacement: '-',  // replace spaces with replacement character, defaults to `-`
+                remove: undefined, // remove characters that match regex, defaults to `undefined`
+                lower: true,      // convert to lower case, defaults to `false`
+                strict: false,     // strip special characters except replacement, defaults to `false`
+                locale: 'vi',       // language code of the locale to use
+                trim: true         // trim leading and trailing replacement chars, defaults to `true`
+            });
+        }
+    },
+};
