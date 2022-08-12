619f222ad7360000bbd8492df8f908ff338477ad
nhtrung1980 <nhtrung1980@gmail.com>
2022-08-12 23:40:05 +0700

    upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,8 @@
 module.exports = ({ env }) => ({
+  editorjs: {
+    enabled: true,
+    resolve: './src/plugins/strapi-plugin-react-editorjs'
+  },
   upload: {
     config: {
       provider: "cloudinary",
@@ -6,6 +10,29 @@ module.exports = ({ env }) => ({
         cloud_name: env("CLOUDINARY_NAME"),
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
+      },
+    },
+  },
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "couchbase",
+        max: 32767,
+        options: {
+          connectionString: "couchbase://127.0.0.1:8091",
+          connectionOptions: {
+            username: "nhtrung1980@gmail.com",
+            password: "04UCth664@@",
+          },
+          bucket: "test-bucket",
+          ttl: 2,
+        },
+      },
+      strategy: {
+        contentTypes: [
+          "api::post.post",
+          "api::term.term",
+        ],
       },
     },
   },
